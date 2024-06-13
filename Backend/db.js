0a245187741d9db1d3844969986853effdce0c68
Nirbhay Walia <nwalia087@gmail.com>
2024-06-13 19:36:03 +0530

    commit

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 const mongoUri = "mongodb://localhost:27017/";
 
-const connectToMongo = () => {
-  mongoose.connect(mongoUri, () => {
-    console.log("connected to mongo succesfully");
-  });
+mongoose.connection.on("connected", () => {
+  console.log("mongoose connected to ", mongoUri);
+});
+mongoose.connection.on("error", (err) => {
+  console.log("mongoose connection error ", err);
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoose disconnected");
+});
+const connectToMongo = async () => {
+  try {
+    mongoose.connect(mongoUri );
+  } catch (err) {
+    console.log("error connecting to mongoDB ", err);
+  }
 };
 
 module.exports = connectToMongo;
