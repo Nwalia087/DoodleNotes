beaae9b4210a75dd229fff58dc5e0dc205a1a73e
Nirbhay Walia <nwalia087@gmail.com>
2024-06-16 15:52:13 +0530

    just before Notes endpoints

---
+++ Backend/middleware/fetchuser.js
@@ -0,0 +1,19 @@
+var jwt = require("jsonwebtoken");
+const JWT_SECRET = "mER@mEOWg#AR@AYA";
+
+const fetchuser = (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send("please enter valid token");
+  }
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    console.log(data);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "please enter valid token" });
+  }
+};
+
+module.exports = fetchuser;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const fetchuser = require("../middleware/fetchuser");
 var jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "mER@mEOWg#AR@AYA";
@@ -41,9 +42,51 @@ router.post(
       res.json(token);
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("breakdown hogya bro");
+      res.status(500).send("internal server error");
     }
   }
 );
+
+router.post(
+  "/login-user",
+  [body("Username", "enter a valid Email").isEmail(), body("password", "password cannot be blank").exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { Username, password } = req.body;
+    try {
+      const user = await User.findOne({ Username });
+      if (!user) {
+        res.status(400).json("please enter valid credentials");
+      }
+      const passCompare = await bcrypt.compare(password, user.password);
+      if (!passCompare) {
+        res.status(400).json("please enter valid credentials");
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      var token = jwt.sign(data, JWT_SECRET);
+      res.json(token);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+router.post("/get-user", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 
 module.exports = router;
