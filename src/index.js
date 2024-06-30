946540d3d19bb7f74db2d1b08f1ed478be2e3666
Nirbhay Walia <nwalia087@gmail.com>
2024-06-30 22:16:15 +0530

    firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6iXDq5FpL8bUZJTsI7KGDPzao4IaYjfU",
+  authDomain: "doodlenotes-4dd7e.firebaseapp.com",
+  projectId: "doodlenotes-4dd7e",
+  storageBucket: "doodlenotes-4dd7e.appspot.com",
+  messagingSenderId: "450247543877",
+  appId: "1:450247543877:web:39050a53adb67e8480672c",
+  measurementId: "G-CE98B2R6B9",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
